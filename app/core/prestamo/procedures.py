a147ed784db4a169f42808855a683cdaf55bfaa3
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2025-11-03 20:05:22 -0300

    Se corrige el procedimiento almacenado sp_trx504 para incluir el parámetro cod_usuario, que se obtiene del usuario autenticado en la solicitud. Este cambio asegura que el código del usuario se pase correctamente al procedimiento almacenado durante la ejecución.

--- app/core/prestamo/procedures.py
+++ app/core/prestamo/procedures.py
@@ -281,7 +281,7 @@ def sp_trx504(request):
     params["nro_solicitud"] = TEXTO(request.POST["solicitud"])
     params["fec_ult_desembolso"] = TEXTO(request.POST["fec_ult_desembolso"])
     params["fec_1er_vencimiento"] = TEXTO(request.POST["fec_1er_vencimiento"])
-    params["cod_usuario"] = cod_usuario
+    params["cod_usuario"] = TEXTO(cod_usuario)
     print(params)
     storedProc = f"""
                     DECLARE @RC INT
@@ -294,7 +294,7 @@ def sp_trx504(request):
                     -- TODO: Establezca los valores de los parámetros aquí.
                     --BEGIN TRAN
                     EXECUTE @RC = [dbo].[TRX_504] 
-                    @NRO_SOLICITUD = {params['nro_solicitud']}
+                     @NRO_SOLICITUD = {params['nro_solicitud']}
                     ,@FEC_ULT_DESEMBOLSO ={params['fec_ult_desembolso']}
                     ,@FEC_1ER_VENCIMIENTO={params['fec_1er_vencimiento']}
                     ,@COD_USUARIO={params['cod_usuario']}
